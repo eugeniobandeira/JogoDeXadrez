1a5b475bac07872148d7ae34bbf8c1678b95a0bf
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-09-11 08:25:19 -0300

    Criando a classe Posicao

---
+++ Xadrez_Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3,4);
+            Console.WriteLine(P);
+
+        }
+    }
+}
