7a4e9fb0510502623d1c3372b85ef9557234c639
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-09-12 17:41:27 -0300

    Testes iniciais com movimentos possíveis das peças

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using tabuleiro;
+﻿using System;
+using tabuleiro;
 using xadrez;
 
 namespace xadrez_console
@@ -21,6 +22,13 @@ namespace xadrez_console
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
--- Xadrez_Console/Tabuleiro/Posicao.cs
+++ Xadrez_Console/Tabuleiro/Posicao.cs
@@ -11,7 +11,11 @@
             this.linha = linha;
             this.coluna = coluna;
         }
-
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
         public override string ToString()
         {
             return linha
