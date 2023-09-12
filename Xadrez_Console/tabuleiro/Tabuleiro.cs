56569f984a4cda8edbf3d45a1d86aaacb438cc1d
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-09-12 10:59:33 -0300

    Criando excecão personalizada para posição de peça

--- Xadrez_Console/tabuleiro/Tabuleiro.cs
+++ Xadrez_Console/tabuleiro/Tabuleiro.cs
@@ -17,10 +17,34 @@ namespace Xadrez_Console.tabuleiro
         {
             return pecas[linha, colunas];
         }
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool ExistePecaNaPosicao(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void ColocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao(Posicao pos)
+        {
+            if(!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
